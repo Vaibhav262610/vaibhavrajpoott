0f211a9bd11d668746605c171f14f40607e002c0
Vaibhav262610 <branda56235@gmail.com>
2025-05-03 02:38:47 +0530

    H

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         {children}
       </body>
     </html>
